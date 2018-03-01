1f674fd75556372f5bb75cd6ebc698d4f2356a06
thinking_mo <split@live.cn>
2018-03-01 23:37:38 +0800

    [项目搭建]

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>KSX | Login</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+
+                <h1 class="logo-name">KSX</h1>
+
+            </div>
+            <h3>欢迎使用速销平台</h3>
+          
+         
+            <form class="m-t" role="form" action="index.html">
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="账号" required="">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="密码" required="">
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b">登录</button>
+
+                <a href="#"><small>忘记密码？</small></a>
+                <p class="text-muted text-center"><small>还没有账号？</small></p>
+                <a class="btn btn-sm btn-white btn-block" href="register.html">注册账号</a>
+            </form>
+            <p class="m-t"> <small>注册成为分销商，月入过万</small> </p>
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ src/util/index.js
@@ -0,0 +1,8 @@
+import AxiosPlugin from './axios.js';
+Vue.use(AxiosPlugin);
+
+import LodashPlugin from './lodash.js';
+Vue.use(LodashPlugin);
+
+import MomentPlugin from './moment.js';
+Vue.use(MomentPlugin);
