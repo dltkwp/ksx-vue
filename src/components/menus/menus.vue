44b18920a30070625245546793e16e9962484bcb
thinking_mo <split@live.cn>
2018-03-04 10:57:10 +0800

    确定 左侧菜单和定位

--- src/components/menus/menus.vue
+++ src/components/menus/menus.vue
@@ -7,9 +7,9 @@
             <a data-toggle="dropdown" class="dropdown-toggle" href="javascript:;;"> 
               <span class="clear"> 
                 <span class="block m-t-xs"> 
-                  <strong class="font-bold">王杰</strong>
+                  <strong class="font-bold">{{userInfo.userName}}</strong>
                 </span> 
-                <span class="text-muted text-xs block">供货商 <b class="caret"></b></span> </span> </a>
+                <span class="text-muted text-xs block">{{userInfo.userType===1?'供应商':'分销商'}} <b class="caret"></b></span> </span> </a>
             <ul class="dropdown-menu animated fadeInRight m-t-xs">
               <li><a href="javascript:;;">退出</a></li>
             </ul>
@@ -40,12 +40,12 @@
             <li><a href="product-distributor.html">商品管理</a></li>
           </ul>
         </li>
-        <li> <a href="javascript:;;">
+        <li v-bind:class="{active:parentKey==='Setting'}"> <a href="javascript:;;">
           <i class="fa fa-gears"></i> 
           <span class="nav-label">设置</span> 
           <span class="fa arrow"></span></a>
           <ul class="nav nav-second-level collapse">
-            <li><a href="setting-info.html">个人信息</a></li>
+            <li v-bind:class="{active:childrenKey==='User'}"><router-link to="/v_user">个人信息</router-link></li>
           </ul>
         </li>
       </ul>
@@ -54,10 +54,36 @@
 </template>
 
 <script>
+  import Vue from 'vue';
   export default {
-    components: {
-      
+    data(){
+      return {
+        parentKey:'Index',
+        childrenKey:'Index',
+        userInfo:{
+          userName:'',
+          userType:''
+        }
+      }
     },
-    props: ["isLoading"]
+    mounted (){
+      let _this = this;
+      let userInfo = localStorage.getItem('ksx-user-c');
+      if(!_this.$lodash.isEmpty(userInfo)){
+        _this.userInfo.userName = userInfo.realname;
+        _this.userInfo.userType = userInfo.userType;
+      } else {
+        _this.$axios.get('user').then((result)=> {
+            let res = result.data;
+            localStorage.setItem('ksx-user-c', JSON.stringify(result.data) );
+            _this.userInfo.userName = res.realname;
+            _this.userInfo.userType = res.userType;
+        }).catch((err) => {});
+      }
+      let meta = _this.$route.meta;
+      _this.parentKey   = meta.parentKey;
+      _this.childrenKey = meta.childrenKey; 
+      console.log(meta);
+    }
   };
 </script>
--- src/router/user.js
+++ src/router/user.js
@@ -3,5 +3,9 @@ import User from '@/page/user/user.vue'
 export default [{
     path: '/v_user',
     name: 'User',
+    meta: {
+        parentKey: 'Setting',
+        childrenKey: 'User',
+    },
     component: User
 }]
